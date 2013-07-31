6af9e81f1ba8cf4476b708e9ed93649a74b2c8be
Jourdan Harvey <jourdan.harvey@gmail.com>
2013-07-31 16:52:45 +1200

    added the ability receive nfc tech, get card id and request user data for the card id from the server

--- src/com/receiptspp/shopfrontmockup/Common/Keys.java
+++ src/com/receiptspp/shopfrontmockup/Common/Keys.java
@@ -1,11 +1,11 @@
 package com.receiptspp.shopfrontmockup.common;
 
 public class Keys {
-	
+
 	// Product Keys
 	public static final String productTitle = "name";
 	public static final String productPrice = "price_per_item";
-	
+
 	// Receipt Keys
 	public static final String receiptStoreName = "store_name";
 	public static final String receiptPhone = "phone";
@@ -15,11 +15,16 @@ public class Keys {
 	public static final String receiptItem = "item";
 	public static final String receiptItems = "items";
 	public static final String receiptItemQuantity = "quantity";
-	public static final String receiptUserId = "userId";
 	public static final String receiptDateTime = "date_time";
 	public static final String receiptCategory = "category";
-	
+
 	// Endpoints
-	public static final String receiptsEndpointStart = "http://receiptspp.herokuapp.com/users/";
+	public static final String receiptsBaseUrl = "http://receiptspp.herokuapp.com/";
+	public static final String receiptsEndpointStart = receiptsBaseUrl
+			+ "users/";
 	public static final String receiptsEndpointEnd = "/receipts";
+	public static final String receiptsSmartcard = receiptsBaseUrl
+			+ "user_from_smartcard/";
+
+	public static final String userId = "user_id";
 }
--- src/com/receiptspp/shopfrontmockup/Common/Util.java
+++ src/com/receiptspp/shopfrontmockup/Common/Util.java
@@ -1,7 +1,22 @@
 package com.receiptspp.shopfrontmockup.common;
 
 public class Util {
-	public static String generatePriceString(double price){
+	public static String generatePriceString(double price) {
 		return String.format("$%.2f", price);
+	}
+
+	public static String getHex(byte[] bytes) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = bytes.length - 1; i >= 0; --i) {
+			int b = bytes[i] & 0xff;
+			if (b < 0x10)
+				sb.append('0');
+			sb.append(Integer.toHexString(b));
+			// Commented out to remove spaces
+			// if (i > 0) {
+			// sb.append(" ");
+			// }
+		}
+		return sb.toString();
 	}
 }
